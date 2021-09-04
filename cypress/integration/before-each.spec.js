f888c30bf3140fde9f61a3937030e021996dcd59
SuperJuice <wisconsinsara@gmail.com>
2021-09-04 16:48:16 -0700

    Added MoreAliasing.spec.js and beforeEach.spec.js

---
+++ cypress/integration/before-each.spec.js
@@ -0,0 +1,31 @@
+beforeEach(() => {
+    cy.visit('http://localhost:3000/example-3');
+});
+
+describe('Textbox allows the appropriate number of characters', () => {
+    it('should display the correct count of remaining characters', () => {
+        // Displays 15 remaining chars immediately after loading webpage.
+        cy.get('[data-cy="first-name-chars-left-count"]').as('firstNameCharsRemainingSpan');
+        cy.get('@firstNameCharsRemainingSpan')
+            .then($firstNameCharsRemainingSpan => {
+                expect($firstNameCharsRemainingSpan.text()).to.equal('15');
+            });
+        
+        // Displays 14 remaining chars after typing 1 char.
+        cy.get('[data-cy="input-first-name"]').type('H');
+        cy.get('@firstNameCharsRemainingSpan').invoke('text').should('equal', '14');
+
+        // Displays 1 remaining char after typing 14 chars total.
+        cy.get('[data-cy="input-first-name"]').type('ello, my frie');
+        cy.get('@firstNameCharsRemainingSpan').invoke('text').should('equal', '1');
+
+        // Displays 0 remaining chars after typing 15 chars total.
+        cy.get('[data-cy="input-first-name"]').type('n');
+        cy.get('@firstNameCharsRemainingSpan').invoke('text').should('equal', '0');
+    })
+
+    it('should prevent the user from typing more than the max num of characters', () => {
+        cy.get('[data-cy="input-last-name"]').type('abcdefghijklmnop');
+        cy.get('[data-cy="input-last-name"]').should('have.attr', 'value', 'abcdefghijklmno');
+    })
+})
